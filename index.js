060b7dd22ef5f4c2d97c4aa2b8f6fe694b69ff79
i.maslakoff <igormaslakoff@gmail.com>
2021-02-17 21:55:53 +0300

    add two sections

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ const sections = [
    { type: 'columns', value: [ loremText, loremText, loremText ] }
 ];
 
+const $content = document.querySelector('#cv');
+
+sections.forEach(section => {
+    let html = '';
+
+    if (section.type === 'title') {
+        html = `
+            <section class="row">
+                <div class="col-sm">
+                    <h2>${section.value}</h2>
+                </div>
+            </section>
+        `;
+    } else if (section.type === 'text') {
+        html = `
+        <section class="row">
+            <div class="col-sm">
+                <p>${section.value}</p>
+            </div>
+        </section>
+        `;
+    } else if (section.type === 'columns') {
+
+    }
+
+    $content.insertAdjacentHTML('beforeend', html)
+})
