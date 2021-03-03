960b2543e7eb205f5a41a08491fe31b8bafa9642
i.maslakoff <igormaslakoff@gmail.com>
2021-03-03 21:24:41 +0300

    events
    Co-authored-by: A-Dulevich <A-Dulevich@users.noreply.github.com>
    Co-authored-by: Dmitri Urbanovich <dimaurbanovich@users.noreply.github.com>
    Co-authored-by: Ivantsov-aa <Ivantsov-aa@users.noreply.github.com>
    Co-authored-by: rominaghanbari@gmail.com <rominaghanbari@gmail.com>

--- src/classes/app.js
+++ src/classes/app.js
@@ -4,8 +4,22 @@ export class Application {
     }
 
     render(sections) {
+
+        let mobileNavigation = document.createElement('i');
+        mobileNavigation.className  = "bi bi-list mobile-nav-toggle";
+
+        const onMobileNavigationClick = () => {
+            document.body.classList.toggle('mobile-nav-active');
+            mobileNavigation.classList.toggle('bi-list');
+            mobileNavigation.classList.toggle('bi-x');
+        }
+
+        mobileNavigation.addEventListener('click', onMobileNavigationClick);
+        this.$content.prepend(mobileNavigation);
+
         sections.forEach(section => {
             this.$content.insertAdjacentHTML('beforeend', section.toHTML())
         })
     }
 }
+
